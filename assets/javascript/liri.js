5bdb64ea77a93026dbe73c1c7cff7d0b0dda2df5
Dan Correa <dancorrea3@gmail.com>
2019-04-22 16:27:00 -0400

    moved spotify variable down

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
 
 var keys = require("./keys.js");
 var spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
