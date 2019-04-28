dd476cfd0736da6b7940330b63368d7cf0d78858
Dan Correa <dancorrea3@gmail.com>
2019-04-27 23:25:52 -0400

    LIRI update

--- liri.js
+++ liri.js
@@ -1,104 +1,127 @@
 var axios = require("axios");
-var Spotify = require('node-spotify-api');
-var keys = require("./keys.js");
+
+// 
+// 
 var moment = require("moment");
-var spotify = new Spotify(keys.spotify);
+// 
+// 
+// 
+// 
+// 
+// getting some weird things going on in these lines(8-18). They keep throwing up errors referring to a function called "spotifyGet()" and a variable named "command2"
+// that no longer exist here. It says that theyve been declared but their values arent being read. 
+// 
+// 
+// 
+// 
+var Spotify = require("node-spotify-api");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function spotifyGet(songInput) {
+function spotifyThisSong() {
     var songInput = process.argv[3];
     if (!songInput) {
-        songInput = 'All the Small Things';
+        songInput = "the+sign"
     }
-    spotify
-        .search({
-            type: 'track',
-            query: song
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (err) {
-            console.log(err);
+    Spotify.search({
+            type: "track",
+            query: songInput
+        },
+
+        function (error, data) {
+            if (!error) {
+                var songData = data.tracks.items[i];
+                console.log("Artist: " + songData.artists[0].name);
+                console.log("Song: " + songData.name);
+                console.log("Preview URL: " + songData.preview_url);
+                console.log("Album: " + songData.album.name);
+                console.log("-----------------------");
+            } else {
+                console.log('Error occurred.');
+            }
         });
-}
+};
 
-// * Title of the movie.
-//   * Year the movie came out.
-//   * IMDB Rating of the movie.
-//   * Rotten Tomatoes Rating of the movie.
-//   * Country where the movie was produced.
-//   * Language of the movie.
-//   * Plot of the movie.
-//   * Actors in the movie.
 
-function movieThis(userInput) {
-    var JSON = JSON.stringify();
+
+
+
+
+
+
+function movieThis() {
     var movieInput = process.argv[3];
-    var omdbUrl = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=short&apikey=trilogy"
-    axios.get(omdbUrl).then(
+    if (!movieInput) {
+        movieInput = "Mr. Nobody";
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl).then(
         function (response) {
             console.log("-----------------Searching for " + movieInput + "-----------------------------");
-            console.log("Release Year: " + response.data.Year);
-            console.log("Movie Rating: " + response.data.JSON(Ratings[1]);
-        });
-    for (var i = 2; i < movieInput.length; i++) {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("Rated: " + response.data.Rated);
+            console.log("Released: " + response.data.Released);
+            console.log("Runtime: " + response.data.Runtime);
+            console.log("Rotten Tomatoes Rating: " + JSON.stringify(response.data.Ratings[1].Value));
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("------------------------------------------------------------------------------");
+        },
 
-        if (i > 2 && i < movieInput.length) {
-            userInput = userInput + "+" + movieInput[i];
-        } else {
-            userInput += movieInput[i];
+        function (error) {
+            console.log(error)
         }
-    }
+    );
+};
+
+function bandtThis() {
+    var bandInput = process.argv[3];
+    var queryUrl = "https://rest.bandsintown.com/artists/" + bandInput + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        function (response) {
+            var datetime = response.data[0].datetime;
+            var randomFormat = "MM/DD/YYYY";
+            var convertedDate = moment(datetime).format(randomFormat);
+            console.log("-----------------Searching for " + bandInput + "-----------------------------");
+            console.log(queryUrl);
+            console.log("Name of the Venue: " + response.data[0].venue.name);
+            console.log("Venue Location: " + response.data[0].venue.city);
+            console.log("Date of the Event: " + convertedDate);
+            console.log("------------------------------------------------------------------------------");
+
+        },
+
+        function (error) {
+            console.log(error)
+        }
+    );
 }
 
-
-function bandThis(artist) {
-    var artist = process.argv[3];
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-        .then(response => {
-            console.log("Venue Name: " + jsonData.venue.name);
-            console.log("Venue Location: " + jsonData.venue.city);
-            console.log("Time")
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
 
 function liri() {
     if (process.argv[2] === 'spotify-this') {
-        console.log("hello in spotify-this");
-        spotifyGet(songInput);
+        spotifyThisSong();
     } else if (process.argv[2] === 'movie-this') {
-        console.log("...executing movie-this")
         movieThis();
+    } else if (process.argv[2] === 'band-this') {
+        bandtThis();
     } else {
-        console.log("...did not recognize command");
+        console.log("------------command not recognized------------");
     }
 }
 
 liri();
+
+function doThis() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        let dataArr = data.split(",");
+
+        userInput = dataArr[0];
+        userQuery = dataArr[1];
+        userCommand(userInput, userQuery);
+    });
+};
